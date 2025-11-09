c3b91583313a254b81a51327969dce935af1efff
John <johnodubayo@gmail.com>
2025-11-09 23:59:26 +0100

    Synced the copies

--- src/DisplayResult.tsx
+++ src/DisplayResult.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import FileAIResponse from "./FileAIResponse";
-import MessageBox from "./messagebox";
+import MessageBox from "./MessageBox";
 type displayProps = {
   files: File[];
   option: string;
